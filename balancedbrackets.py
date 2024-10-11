bd9c9cc44974008892008114ea93793ff7c2e5b9
Karla Carrera <kcarrera@sea.yearup.org>
2024-10-10 21:00:21 -0700

    fixed balancedbrackets file

--- balancedbrackets.py
+++ balancedbrackets.py
@@ -1,22 +1,35 @@
+# defining the function
 def has_balanced_brackets(phrase):
     """Does a given string have balanced pairs of brackets?
 
     Given a string as input, return True or False depending on whether the
     string contains balanced (), {}, [], and/or <>.
     """
+    #keeps track of opening and closing brackets
     stack = []
-    brackets = {')': '(', '}': '{', ']': '['}
+    #Dictionary to corresponding brackets
+    brackets = {
+    ')': '(', 
+    '}': '{', 
+    ']': '['
+    }
 
+#begin loop through the charaters in phrase
     for char in phrase:
+#adding character to the stack list (opening brackets)
         if char in brackets.values():
             stack.append(char)
+
+#checking for closing brackets
         elif char in brackets.keys():
+#if no characters in stack or unclosed openeing bracket return false
             if not stack or stack.pop() != brackets[char]:
                 return False
-            else:
-                continue
-        return not stack
+# return not stack was inside the loop
+    return not stack
     
-    print(has_balanced_brackets("([]{})"))  # True
+print(has_balanced_brackets("([]{})"))  # True
 print(has_balanced_brackets("([)]"))  # False
 print(has_balanced_brackets("{{[()]}}"))  # True
+print(has_balanced_brackets("((hello world)")) #False
+print(has_balanced_brackets("({[this is not balanced})"))
---
+++ mostactive.py
@@ -0,0 +1,2 @@
+def most_active(bio_data):
+    """Find window of time when most authors were active."""
